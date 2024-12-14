d0c8ab13d93a3b6d233a6b274e643db895b2c01a
vmp010 <B11223053@gemail.yuntech.edu.tw>
2024-12-14 15:36:57 +0800

    addBorrowBookFunctionInUser.java

--- User.java
+++ User.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 class User {
     private int uid;
     private String uname;
     private String upassword;
+    private List<Book> borrowedBooks;
 
     // 建構子
     public User(int uid, String uname, String upassword) {
         this.uid = uid;
         this.uname = uname;
         this.upassword = upassword;
+        this.borrowedBooks = new ArrayList<>();
     }
     public int getUid(){
         return uid;
@@ -18,9 +22,22 @@ class User {
     public String getUpassword(){
         return upassword;
     }
+    public List<Book> getBorrowedBooks(){
+        return borrowedBooks;
+    }
     // 將 User 轉為 CSV 行
     public String toCsvRow() {
         return uid + "," + uname + "," + upassword;
+    }
+    //借書功能
+    public void borrowBook(Book book){
+        if(!book.getIsBorrowed()){ //檢查書本是否已被借出
+            this.borrowedBooks.add(book);
+            book.borrowBook(); //更新書本狀態
+            System.out.println(this.uname+"成功借了"+book.getBookName());
+        }else{
+            System.out.println(book.getBookName()+"此書已被借走");
+        }
     }
     //解析 CSV 文件中的行
     public static User fromCsvRow(String csvRow){
